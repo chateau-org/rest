79b2517f689f0a9da5161692b5404b1d1125e989
Frolleks <frolleks@hotmail.com>
2023-01-04 18:33:00 +0700

    feat: use argon2 instead of bcrypt

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,36 +1,31 @@
 import express from "express";
-import bcrypt from "bcrypt";
 import User from "../models/auth";
 import mongoose from "mongoose";
+import argon2 from "argon2";
 import log from "../utils/logger/logger";
 
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
-      return res.status(500).json({ error: err });
-    } else {
-      const user = new User({
-        _id: new mongoose.Types.ObjectId,
-        email: req.body.email,
-        password: hash,
-      });
-      user
-        .save()
-        .then(result => {
-          res.status(201).json({
-            message: "User successfully created"
-          });
-        })
-        .catch(err => {
-          log.error(err);
-          res.status(500).json({
-            error: err
-          })
-        });
-    }
+router.post("/signup", async (req, res) => {
+  const hash = await argon2.hash(req.body.password);
+  const user = new User({
+    _id: new mongoose.Types.ObjectId(),
+    email: req.body.email,
+    password: hash,
   });
+  user
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "User successfully created",
+      });
+    })
+    .catch((err) => {
+      log.error(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 export default router;
