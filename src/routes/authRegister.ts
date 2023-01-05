4de3cd61717dfb3e8c2515cfeec30228d19c1db7
Frolleks <frolleks@hotmail.com>
2023-01-05 20:21:42 +0700

    feat: initial jwt support

--- src/routes/authRegister.ts
+++ src/routes/authRegister.ts
@@ -3,8 +3,12 @@ import User from "../models/auth";
 import mongoose from "mongoose";
 import argon2 from "argon2";
 import validator from "validator";
+import jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
 
 const router = express.Router();
+
+dotenv.config();
 
 router.post("/", async (req, res) => {
   const hash = await argon2.hash(req.body.password);
@@ -16,9 +20,13 @@ router.post("/", async (req, res) => {
   if (validator.isEmail(user.email) === true) {
     user
       .save()
-      .then(() => {
+      .then((result) => {
+        // @ts-ignore
+        const jwtToken = jwt.sign({ data: result }, process.env.JWT_TOKEN, {
+          expiresIn: "1h",
+        });
         res.status(201).json({
-          message: "User successfully created",
+          message: "User sucessfully created"
         });
       })
       .catch((err) => {
